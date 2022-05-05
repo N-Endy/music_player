af3871b1181aec4220f0a119de5375c2b8bd13f7
Okafor Nelson <okafornel@gmail.com>
2022-05-05 16:14:04 +0100

    Make intent work on PlayActivity

--- app/src/main/java/com/example/android/musicplayer/PlayActivity.java
+++ app/src/main/java/com/example/android/musicplayer/PlayActivity.java
@@ -3,12 +3,32 @@ package com.example.android.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_song_list);
+        setContentView(R.layout.activity_play);
+
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent() {
+        if(getIntent().hasExtra("songTitle") && getIntent().hasExtra("artisteName")) {
+            String songTitle = getIntent().getStringExtra("songTitle");
+            String artisteName = getIntent().getStringExtra("artisteName");
+
+            setIncomingIntent(songTitle, artisteName);
+        }
+    }
+
+    private void setIncomingIntent(String songTitle, String artisteName) {
+        TextView title = findViewById(R.id.play_song_title);
+        title.setText(songTitle);
+        TextView name = findViewById(R.id.play_song_artiste);
+        name.setText(artisteName);
     }
 }
